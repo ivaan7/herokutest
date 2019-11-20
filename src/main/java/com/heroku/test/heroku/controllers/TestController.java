57ff9a52f9b46a1b2af47bca85c3606183757273
Ivan Bosnjakovic <Ivan.Bosnjakovic@infobip.com>
2019-11-20 10:50:05 +0100

    fix

--- src/main/java/com/heroku/test/heroku/controllers/TestController.java
+++ src/main/java/com/heroku/test/heroku/controllers/TestController.java
@@ -1,20 +1,21 @@
 package com.heroku.test.heroku.controllers;
 
-import com.heroku.test.heroku.model.TestRequestBody;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class TestController {
 
-    @PostMapping("/herokutest")
-    public String test(@RequestBody TestRequestBody body, @RequestHeader HttpHeaders httpHeaders) {
+    @RequestMapping("/message")
+    public String test(@RequestBody byte[] body, HttpServletRequest httpServletRequest, @RequestHeader HttpHeaders httpHeaders) {
 
+        System.out.println(httpServletRequest.getMethod() + " " + httpServletRequest.getRequestURI());
+        System.out.println(Hex.encodeHexString(body));
         System.out.println(httpHeaders);
-        System.out.println(body);
+
         return "ok";
     }
 
